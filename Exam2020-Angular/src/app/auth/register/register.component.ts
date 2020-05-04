84c6227cc223830c4c0775bf82a66c208b78ed18
mapoul <44639698+mapoul@users.noreply.github.com>
2020-05-04 12:26:35 +0200

    Update register.component.ts

--- Exam2020-Angular/src/app/auth/register/register.component.ts
+++ Exam2020-Angular/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {User} from '../shared/user';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  userForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    userName: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  register() {
+
+  }
 }

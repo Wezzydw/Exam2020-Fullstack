692fa7d96f3d89719c2c6eb8cb161e2224b56040
andr4229 <andr4229@hotmail.com>
2020-05-04 11:40:05 +0200

    ...

--- Exam2020-CertificateApp/src/app/auth/auth.module.ts
+++ Exam2020-CertificateApp/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AuthRoutingModule} from './auth-routing.module';
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 
@@ -13,6 +14,7 @@ import {AuthRoutingModule} from './auth-routing.module';
     CommonModule,
     AuthRoutingModule,
     AngularFireAuthModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthModule { }

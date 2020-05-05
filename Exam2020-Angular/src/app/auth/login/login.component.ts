9cf03951e673100b35e57a5e5488f545a5567e98
andr4229 <andr4229@hotmail.com>
2020-05-05 11:00:20 +0200

    state setup

--- Exam2020-Angular/src/app/auth/login/login.component.ts
+++ Exam2020-Angular/src/app/auth/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Select, Store} from '@ngxs/store';
 import {AuthState} from '../shared/auth.state';
 import {AuthUser} from '../shared/authUser';
 import {Observable} from 'rxjs';
 import {LoginEmail} from '../shared/auth.action';
+import {AuthService} from "../shared/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -12,20 +13,25 @@ import {LoginEmail} from '../shared/auth.action';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  // loginForm: FormGroup;
   loginForm = new FormGroup({
     email: new FormControl(''),
-    password: new FormControl(''),
+    password: new FormControl('')
   });
   @Select(AuthState.loggedInUser)
   loggedInUser$: Observable<AuthUser>;
-  constructor(private store: Store) { }
+  constructor(private store: Store, private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit() {
+    // this.loginForm = this.formBuilder.group({
+    //   userName: ['', Validators.required],
+    //   password: ['', Validators.required]
+    // });
   }
   loginWithEmail() {
-    const email = '';
-    const password = '';
+    const email = this.loginForm.get('email').value;
+    const password = this.loginForm.get('password').value;
+    // this.authService.logInEmail(email, password);
     this.store.dispatch(new LoginEmail(email, password));
   }
 }
--- Exam2020-Angular/src/app/users/shared/user.service.ts
+++ Exam2020-Angular/src/app/users/shared/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AuthUser} from '../../auth/shared/authUser';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private af: AngularFirestore) { }
+  getUser(uId: string): Observable<AuthUser> {
+    return this.af.doc<AuthUser>('users/' + uId).snapshotChanges().pipe(
+      map(value => {
+        const data = value.payload.data() as AuthUser;
+        data.uid = value.payload.id;
+        return data;
+      })
+    );
+  }
 }

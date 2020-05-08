0840401e6ba2294d607c654e42b5450a5b6d04cd
mapoul <44639698+mapoul@users.noreply.github.com>
2020-05-08 11:34:07 +0200

    test

--- Exam2020-CertificateApp/functions/test/index.test.ts
+++ Exam2020-CertificateApp/functions/test/index.test.ts
@@ -1,5 +1,4 @@
-// At the top of test/index.test.js
-import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions'
 
 const test = require('firebase-functions-test')({
   databaseURL: 'https://certificate-project-fbc50.firebaseio.com/',
@@ -7,17 +6,21 @@ const test = require('firebase-functions-test')({
   projectId: 'certificate-project-fbc50',
 }, 'functions/certificate-functions-test.json');
 
-const myFunctions = require('../src/index.js'); // relative path to functions code
-const wrapped = test.wrap(myFunctions.profileImageAdded);
-let data = require('C:/Users/mpoul/Pictures/launcher.jpg');
-let data1 = require('C:/Users/mpoul/Pictures/profile.jpg');
+const myFunctions = require('/src/index.ts'); // relative path to functions code
 
-const beforeSnap = test.firestore.makeDocumentSnapshot(data, 'images/SluEwNNVe6gjGmZD1Z3STvLelOa2/profilePicture');
-const afterSnap = test.firestore.makeDocumentSnapshot(data1, 'images/SluEwNNVe6gjGmZD1Z3STvLelOa2/profilePicture');
-const change = test.makeChange(beforeSnap, afterSnap);
-return wrapped(change).then(() => {
-return admin.database().ref('images/SluEwNNVe6gjGmZD1Z3STvLelOa2/profilePicture').once('value').then((createdSnap) => {
-  assert.equal(createdSnap.val(), data1);
-  });
+describe('Cloud Functions', () => {
+
+  const wrapped = test.wrap(myFunctions.profileImageAdded);
+  let data = require('C:/Users/mpoul/Pictures/launcher.jpg');
+  let data1 = require('C:/Users/mpoul/Pictures/profile.jpg');
+  wrapped(data);
+  const beforeSnap = test.firestore.makeDocumentSnapshot(data, 'images/SluEwNNVe6gjGmZD1Z3STvLelOa2/profilePicture');
+  wrapped(data1);
+  const afterSnap = test.firestore.makeDocumentSnapshot(data1, 'images/SluEwNNVe6gjGmZD1Z3STvLelOa2/profilePicture');
+  const change = test.makeChange(beforeSnap, afterSnap);
+  // return wrapped(change).then(() => {
+  //   return admin.database().ref('images/SluEwNNVe6gjGmZD1Z3STvLelOa2/profilePicture').once('value').then((createdSnap) => {
+  //     assert.equal(createdSnap.val(), data1);
+  //   });
+  // });
 });
-

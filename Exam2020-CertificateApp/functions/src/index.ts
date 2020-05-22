50fb183bc3999b81a976214e76d1d88c24fa9eb1
andr4229 <andr4229@hotmail.com>
2020-05-22 14:21:39 +0200

    functions for on delete

--- Exam2020-CertificateApp/functions/src/index.ts
+++ Exam2020-CertificateApp/functions/src/index.ts
@@ -24,3 +24,34 @@ exports.profileImageAdded = functions.storage.object().onFinalize((object) => {
     }
   })
 });
+exports.onUserDelete = functions.auth.user().onDelete(user => {
+  admin.firestore().collection('users').doc(user.uid).delete().then().catch();
+  admin.firestore().collection('certificates').where('mUserUid','==',user.uid).get().then(value => {
+    value.forEach(result => {
+      admin.firestore().collection('certificates').doc(result.data().mUId).delete().then().catch();
+    })
+  }).catch();
+  const bucket = admin.storage().bucket();
+  bucket.deleteFiles({prefix: 'images/'+user.uid+'/'},
+    function (err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(`All the Firebase Storage files in  have been deleted`);
+    }
+    })
+});
+exports.onCertificateDelete = functions.firestore.document('certificates/{uid}').onDelete((snapshot, context) => {
+  return new Promise(async (resolve, reject) => {
+    const deletedValue = snapshot.data();
+    if (deletedValue) {
+      try {
+        const resultFromStorage = await admin.storage().bucket().file('images/' + deletedValue.mUserUid + '/certificates/' + deletedValue.mUId).delete().then();
+        resolve(resultFromStorage);
+      } catch (e) {
+        reject(e);
+      }
+    }
+  });
+});
+

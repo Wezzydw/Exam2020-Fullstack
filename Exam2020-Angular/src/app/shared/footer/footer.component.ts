ebc60f832e9e1f0180a6de74732355cc712d21a2
mapoul <44639698+mapoul@users.noreply.github.com>
2020-05-25 15:21:51 +0200

    asd

--- Exam2020-Angular/src/app/shared/footer/footer.component.ts
+++ Exam2020-Angular/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {News} from '../models/news';
+import {NewsService} from '../services/news.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  news: News[];
 
-  constructor() { }
+  constructor(private newsService: NewsService) { }
 
   ngOnInit() {
+    this.getAllNews();
   }
 
+  getAllNews() {
+    this.newsService.getAllNews().subscribe(a => {
+      this.news = a;
+    });
+  }
 }
--- Exam2020-Angular/src/app/shared/services/news.service.ts
+++ Exam2020-Angular/src/app/shared/services/news.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {News} from '../models/news';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
+url = 'https://localhost:44326/api/news';
 
-  constructor() { }
+  constructor(private client: HttpClient) { }
+
+  getAllNews(): Observable<News[]> {
+    return this.client.get<News[]>(this.url);
+  }
 }
